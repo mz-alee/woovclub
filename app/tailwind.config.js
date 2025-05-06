b13ce112cd3caa4aba0f4cf64bc7c33dbbc9a740
mz-alee <aliraza.1006724@gmail.com>
2025-05-06 15:43:57 +0500

    fix build error

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,10 @@
-const {heroui} = require('@heroui/theme');
-/** @type {import('tailwindcss').Config} */
-  content: [
-    "./node_modules/@heroui/theme/dist/components/(dropdown|menu|divider|popover|button|ripple|spinner).js"
-],
+
 export default {
   theme: {
     extend: {
       colors: {
-        primary: "#1E40AF", // Custom primary color
-        secondary: "#E11D48", // Custom secondary color
+        primary: "#1E40AF", 
+        secondary: "#E11D48",
       },
       backgroundImage: {
         "hero-pattern": "url('/images/bg.jpg')",
